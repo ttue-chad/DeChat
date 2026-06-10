5718b28b792ec351bf60a9630ac36897ebe3135b
ttue-chad <ttuevsjv@gmail.com>
2026-06-10 09:07:51 +0100

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,1 +1,30 @@
+import { addMessage } from "./ui";
+import { encrypt } from "./crypto";
 
+const sendBtn =
+  document.getElementById("sendBtn")!;
+
+const input =
+  document.getElementById(
+    "messageInput"
+  ) as HTMLInputElement;
+
+sendBtn.addEventListener(
+  "click",
+  async () => {
+
+    const encrypted =
+      await encrypt(input.value);
+
+    addMessage(
+      "You: " + input.value
+    );
+
+    console.log(
+      encrypted
+    );
+
+    input.value = "";
+
+  }
+);
