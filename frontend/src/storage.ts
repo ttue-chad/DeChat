58ea5d7b1755d227c2b620e7f56c75ded2547561
ttue-chad <ttuevsjv@gmail.com>
2026-06-10 09:08:41 +0100

    Update storage.ts

--- frontend/src/storage.ts
+++ frontend/src/storage.ts
@@ -1,1 +1,16 @@
+export async function saveKey(
+  key:string,
+  value:string
+) {
 
+  localStorage.setItem(key, value);
+
+}
+
+export async function loadKey(
+  key:string
+) {
+
+  return localStorage.getItem(key);
+
+}
