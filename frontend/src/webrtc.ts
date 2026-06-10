ee84e96764ad4c3d2e2a8ec4cfc73ace69aefcf1
ttue-chad <ttuevsjv@gmail.com>
2026-06-10 09:10:18 +0100

    Update webrtc.ts

--- frontend/src/webrtc.ts
+++ frontend/src/webrtc.ts
@@ -1,1 +1,44 @@
+export class ChatPeer {
 
+  private pc: RTCPeerConnection;
+  private channel?: RTCDataChannel;
+
+  constructor() {
+
+    this.pc = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: "stun:stun.l.google.com:19302"
+        }
+      ]
+    });
+
+  }
+
+  createChannel() {
+
+    this.channel =
+      this.pc.createDataChannel("chat");
+
+    return this.channel;
+  }
+
+  onMessage(
+    callback:(msg:string)=>void
+  ) {
+
+    this.pc.ondatachannel = (event) => {
+
+      const channel = event.channel;
+
+      channel.onmessage = (msg) => {
+
+        callback(msg.data);
+
+      };
+
+    };
+
+  }
+
+}
